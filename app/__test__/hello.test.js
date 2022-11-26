dd01f4404a474791d61e864f30ff3b71d5e89d99
quotto <5211139+quotto@users.noreply.github.com>
2022-11-26 23:20:38 +0900

    ARMテンプレートとGitHubActionsの最新化

--- app/__test__/hello.test.js
+++ app/__test__/hello.test.js
@@ -1,9 +1,21 @@
 const hello = require("../hello/index.js");
 
 describe("hello", () => {
-   it ("パラメータnameが設定されている",async ()=>{
-       const context = { log: (message) => console.log(message) };
-       await hello(context, { query: {name: "john" }});
-       expect(context.res.body).toBe("Hello, john. This HTTP triggered function executed successfully.");
-   });
+    const context = { log: (message) => console.log(message) };
+    it("パラメータnameがクエリに設定されている", async () => {
+        await hello(context, { query: { name: "john" } });
+        expect(context.res.body).toBe("Hello, john. This HTTP triggered function executed successfully.");
+    });
+    it("パラメータnameがリクエストボディに設定されている", async () => {
+        await hello(context, { query: {}, body: { name: "john" } });
+        expect(context.res.body).toBe("Hello, john. This HTTP triggered function executed successfully.");
+    });
+    it("パラメータnameがクエリにもリクエストボディに設定されている場合はクエリが優先される", async () => {
+        await hello(context, { query: { name: "john" }, body: { name: "mike" } });
+        expect(context.res.body).toBe("Hello, john. This HTTP triggered function executed successfully.");
+    });
+    it("パラメータnameクエリにもリクエストボディにも設定されていない", async () => {
+        await hello(context, { query: {}, body: {}});
+        expect(context.res.body).toBe("This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.");
+    })
 });
