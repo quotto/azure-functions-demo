19f68a67bd63cce03d7e7622d1d8bf0da4939ed0
quotto <5211139+quotto@users.noreply.github.com>
2023-01-02 07:47:03 +0000

    タイポ

--- utils.js
+++ utils.js
@@ -1,10 +1,10 @@
 exports.handleAxiosError = async(context,error) => {
     if (error.response) {
         context.log.error("axios response error");
-        context.log.error(`${error.response.status}: ${typeof (error.response.data) === "object" ? json.stringify(error.response.data) : error.response.data}`);
+        context.log.error(`${error.response.status}: ${typeof (error.response.data) === "object" ? JSON.stringify(error.response.data) : error.response.data}`);
     } else if (error.request) {
         context.log.error("axios request error");
-        context.log.error(`bad request: ${json.stringify(error.request)}`);
+        context.log.error(`bad request: ${JSON.stringify(error.request)}`);
     }
     context.log.error(error.message);
     context.log.error(error.stack);
