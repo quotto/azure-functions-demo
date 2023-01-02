7ac80dfe2c9ff2001e51d97e9db4b25bde6cafc9
quotto <5211139+quotto@users.noreply.github.com>
2023-01-02 02:38:05 +0000

    axiosエラーハンドリングの修正

--- graph.js
+++ graph.js
@@ -3,11 +3,22 @@ module.exports = class GraphRequester {
     static GRAPH_ENDPOINT = "https://graph.microsoft.com/v1.0";
     static PATH_SIGNIN_USER = "/me";
     static PATH_ALL_TENANT_USER = "/users"
-    static async getMe(token) {
+    static async getMe(context, token) {
         const headers = {
             Authorization: `Bearer ${token}`
         }
-        return await axios.get(`${this.GRAPH_ENDPOINT}${this.PATH_SIGNIN_USER}`, {headers: headers}).then(response=>response).catch(error=>error.response)
+        return await axios.get(`${this.GRAPH_ENDPOINT}${this.PATH_SIGNIN_USER}`, {headers: headers}).then(response=>response).catch(error=>{
+            if (error.response) {
+                context.log.error("graph api get me response error");
+                context.log.error(`${error.response.status}: ${typeof (error.response.data) === "object" ? JSON.stringify(error.response.data) : error.response.data}`);
+            } else if (error.request) {
+                context.log.error("graph api get me request error");
+                context.log.error(`Bad request: ${JSON.stringify(error.request)}`);
+            }
+            context.log.error(error.message);
+            context.log.error(error.stack);
+            return { error: true, message: error.message };
+        })
     }
     static async getAllTenantUser(token) {
         const headers = {
--- me/index.js
+++ me/index.js
@@ -1,35 +1,29 @@
 const axios = require("axios");
 const GraphRequester = require("../graph");
+const querystring = require('querystring');
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     context.log(JSON.stringify(req));
     const token = req.headers.authorization.split(" ")[1];
     const bobResponse = await getGraphApiTokkenWithUserPermission(token);
-    if(bobResponse.status === 200) {
+    if(!bobResponse.error && bobResponse.status === 200) {
         const result = await GraphRequester.getMe(bobResponse.data.access_token);
-        if(result.status === 200) {
+        if(!result.erro && result.status === 200) {
             context.res = {
                 // status: 200, /* Defaults to 200 */
-                body: JSON.stringify({
-                    message: JSON.stringify(result.data)
-                })
+                body:  JSON.stringify(result.data)
             };
         } else {
-            context.log.error("graph api request user profile error");
-            context.log.error(`${result.statusText}: ${JSON.stringify(result.data)}`)
             context.res = {
                 status: 500,
-                body: JSON.stringify({message: "request user profile error"})
+                body: JSON.stringify({message: result.message})
             }
         }
     } else {
-        context.log.error("get user token error");
-        context.log.error(JSON.stringify(bobResponse));
-        context.log.error(`${bobResponse.statusText}: ${JSON.stringify(bobResponse.data)}`)
         context.res = {
             status: 500,
-            body: JSON.stringify({message: bobResponse.data.error, description: bobResponse.data.error_description})
+            body: JSON.stringify({message: bobResponse.message})
         }
     }
 }
@@ -47,5 +41,17 @@ const getGraphApiTokkenWithUserPermission = async(token)=>{
         scrop: "https://graph.microsoft.com/User.Read",
         requested_token_use: "on_behalf_of"
     }
-    return await axios.post(`https://login.microsoftonline.com/${process.env.APP_TENANT_ID}/oauth2/v2.0/token`,{headers: headers, data: body}).then(response=>response).catch(error=>error.response)
+    return await axios.post(`https://login.microsoftonline.com/${process.env.APP_TENANT_ID}/oauth2/v2.0/token`,{headers: headers, data: querystring.stringify(body)}).then(response=>response).catch(error=>{
+        if(error.response) {
+            context.log.error("get graph api token with user permission response error");
+            ;
+            context.log.error(`${error.response.status}: ${typeof(error.response.data) === "object" ? JSON.stringify(error.response.data) : error.response.data}`);
+        } else if (error.request) {
+            context.log.error("get graph api token with user permission request error");
+            context.log.error(`Bad request: ${JSON.stringify(error.request)}`);
+        }
+        context.log.error(error.message);
+        context.log.error(error.stack);
+        return {error: true, message: error.message};
+    })
 }
