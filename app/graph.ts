f0b3e26f34bd642be050b68b2aa60a8471d3cae1
quotto <5211139+quotto@users.noreply.github.com>
2023-01-10 06:12:40 +0000

    テストの作成

--- app/graph.ts
+++ app/graph.ts
@@ -1,21 +1,17 @@
 import axios, { AxiosResponse } from "axios";
 import {Context} from "@azure/functions";
-import {ErrorResponse, handleAxiosError} from "./utils";
+import {ErrorResponse, handleAxiosError, logger} from "./utils";
 export class GraphRequester {
     private static GRAPH_ENDPOINT = "https://graph.microsoft.com/v1.0";
     private static PATH_SIGNIN_USER = "/me";
     private static PATH_ALL_TENANT_USER = "/users";
-    private context: Context;
-    constructor(_context: Context) {
-        this.context = _context;
-    }
     async getMe(token: string): Promise<AxiosResponse | ErrorResponse>  {
         const headers = {
             Authorization: `Bearer ${token}`
         }
         return await axios.get(`${GraphRequester.GRAPH_ENDPOINT}${GraphRequester.PATH_SIGNIN_USER}`, {headers: headers}).then(response=>response).catch(error=>{
-            this.context.log.error("get user profile error");
-            return handleAxiosError(this.context,error);
+            logger.error("get user profile error");
+            return handleAxiosError(error);
         })
     }
     async getAllTenantUsers(token: string) {
@@ -23,8 +19,8 @@ export class GraphRequester {
             Authorization: `Bearer ${token}`
         }
         return await axios.get(`${GraphRequester.GRAPH_ENDPOINT}${GraphRequester.PATH_ALL_TENANT_USER}`, {headers: headers}).then(response=>response).catch(error=>{
-            this.context.log.error("get all tenant users error");
-            return handleAxiosError(this.context,error);
+            logger.error("get all tenant users error");
+            return handleAxiosError(error);
         })
     }
 }
--- app/me/index.ts
+++ app/me/index.ts
@@ -1,48 +1,31 @@
-import {Context, HttpRequest} from "@azure/functions";
-import axios,{AxiosResponse} from "axios";
-import {GraphRequester} from "../graph";
-import querystring from 'querystring';
-import { handleAxiosError,ErrorResponse, isErrorResponse } from "../utils";
+import { Context, HttpRequest } from "@azure/functions";
+import { GraphRequester } from "../graph";
+import { ErrorResponse, isErrorResponse, logger } from "../utils";
+import { getGraphApiTokkenWithUserPermission } from "../token-requester";
+import { AxiosResponse } from "axios";
 export default async function (context: Context, req: HttpRequest) {
+    logger.setContext(context);
+
     const token = req.headers.authorization.split(" ")[1];
-    const bobResponse = await getGraphApiTokkenWithUserPermission(context,token);
-    if(!isErrorResponse(bobResponse) && bobResponse.status === 200) {
-        const graph_requester = new GraphRequester(context);
-        const result = await graph_requester.getMe(bobResponse.data.access_token);
-        if(!isErrorResponse(result) && result.status === 200) {
+    const bobResponse = await getGraphApiTokkenWithUserPermission(token);
+    if (!isErrorResponse(bobResponse) && (bobResponse as AxiosResponse).status === 200) {
+        const graph_requester = new GraphRequester();
+        const result = await graph_requester.getMe((bobResponse as AxiosResponse).data.access_token);
+        if (!isErrorResponse(result) && (result as AxiosResponse).status === 200) {
             context.res = {
-                // status: 200, /* Defaults to 200 */
-                body:  JSON.stringify({data: result.data, used_token: bobResponse.data.access_token})
+                status: 200, /* Defaults to 200 */
+                body: JSON.stringify({ data: (result as AxiosResponse).data, used_token: (bobResponse as AxiosResponse).data.access_token })
             };
         } else {
             context.res = {
                 status: 500,
-                body: JSON.stringify({message: (result as ErrorResponse).message})
+                body: JSON.stringify({ message: (result as ErrorResponse).message })
             }
         }
     } else {
         context.res = {
             status: 500,
-            body: JSON.stringify({message: (bobResponse as ErrorResponse).message})
+            body: JSON.stringify({ message: (bobResponse as ErrorResponse).message })
         }
     }
-}
-
-const getGraphApiTokkenWithUserPermission = async(context: Context,token: string): Promise<AxiosResponse | ErrorResponse> =>{
-    const headers = {
-        Authorization: `Bearer ${token}`,
-        ContentType: "application/json"
-    }
-    const body = {
-        grant_type: "urn:ietf:params:oauth:grant-type:jwt-bearer",
-        client_id: process.env.APP_CLIENT_ID,
-        client_secret: process.env.APP_CLIENT_SECRET,
-        assertion: token,
-        scope: "https://graph.microsoft.com/User.Read",
-        requested_token_use: "on_behalf_of"
-    }
-    return await axios.post(`https://login.microsoftonline.com/${process.env.APP_TENANT_ID}/oauth2/v2.0/token`, querystring.stringify(body),{headers: headers}).then(response=>response).catch(error=>{
-        context.log.error("get graph api token with user permission response error");
-        return handleAxiosError(context,error);
-    })
 }
