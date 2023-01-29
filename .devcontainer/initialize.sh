6bdea58e42a9858c923191a43c7e1e85ee51e396
quotto <5211139+quotto@users.noreply.github.com>
2023-01-29 16:42:33 +0900

    initialize.shにsudo追加

--- .devcontainer/initialize.sh
+++ .devcontainer/initialize.sh
@@ -6,9 +6,14 @@ apt-get update
 apt-get -y install --no-install-recommends openvpn \
 apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/library-scripts \
 
+sudo_cmd=""
+if [ "$(id -u)" != "0" ]; then
+    sudo_cmd="sudo"
+fi
+
 # Remove the OPENVPN_CONFIG variable since we don't neeed it after is written to a file
-echo 'OPENVPN_CONFIG=""' >> /etc/environment
-echo "unset OPENVPN_CONFIG" | tee -a /etc/bash.bashrc > /etc/profile.d/999-unset-openvpn-config.sh
+${sudo_cmd} echo 'OPENVPN_CONFIG=""' >> /etc/environment
+${sudo_cmd} echo "unset OPENVPN_CONFIG" | tee -a /etc/bash.bashrc > /etc/profile.d/999-unset-openvpn-config.sh
 if [ -d "/etc/zsh" ]; then
-    echo "unset OPENVPN_CONFIG" >> /etc/zsh/zshenv
+    ${sudo_cmd} echo "unset OPENVPN_CONFIG" >> /etc/zsh/zshenv
 fi
