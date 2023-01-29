1ef8ce571ca8bc36e02438e1f7ff96ea9092f0cd
quotto <5211139+quotto@users.noreply.github.com>
2023-01-29 07:13:21 +0000

    コンテナ内の初期化はonCreateCommandで実施する

--- .devcontainer/initialize.sh
+++ .devcontainer/initialize.sh
@@ -1,5 +1,4 @@
 #!/bin/bash -e
-set -e
 
 # install openvpn client
 export DEBIAN_FRONTEND=noninteractive
@@ -12,17 +11,4 @@ echo 'OPENVPN_CONFIG=""' >> /etc/environment
 echo "unset OPENVPN_CONFIG" | tee -a /etc/bash.bashrc > /etc/profile.d/999-unset-openvpn-config.sh
 if [ -d "/etc/zsh" ]; then
     echo "unset OPENVPN_CONFIG" >> /etc/zsh/zshenv
-fi
-
-
-# Switch to the .devcontainer folder
-cd "$( dirname "${BASH_SOURCE[0]}" )"
-
-# Create a temporary directory
-mkdir -p openvpn-tmp
-cd openvpn-tmp
-
-# Save the configuration from the secret if it is present
-if [ ! -z "${OPENVPN_CONFIG}" ]; then
-    echo "${OPENVPN_CONFIG}" > vpnconfig.ovpn
 fi
